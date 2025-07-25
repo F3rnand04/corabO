b0f274c995f01fb4108da3708c1713840e435459
Fernando Pbt <fernandopbt450@gmail.com>
2025-07-25 19:47:46 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button variant="g

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,7 +19,7 @@ export default function Footer() {
     { href: '/videos', icon: PlaySquare, label: 'Videos' },
     { href: '#upload', icon: Upload, label: 'Añadir', isCentral: true },
     { href: '/messages', icon: MessageSquare, label: 'Mensajes' },
-    { href: '/settings', icon: Settings, label: 'Ajustes' },
+    { href: '/profile', icon: Settings, label: 'Ajustes' },
   ];
 
   return (
