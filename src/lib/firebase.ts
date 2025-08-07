ccbebf293cba236f7c8fbaa65d3b7bffe1c4bfc3
Fernando Pbt <fernandopbt450@gmail.com>
2025-08-07 20:13:39 +0000

    perfecto ya abre pero no me da paso esta haciendo como un rebuild a la i

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,8 @@
+
 "use client";
 
 import { initializeApp, getApp, getApps } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
 
 // Your web app's Firebase configuration - KEEP THIS AS IS FROM THE CONSOLE
 const firebaseConfig = {
@@ -14,8 +15,26 @@ const firebaseConfig = {
   "messagingSenderId": "220291714642"
 };
 
+
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
+
+// Enable Firestore persistence
+if (typeof window !== 'undefined') {
+  enableIndexedDbPersistence(db)
+    .catch((err) => {
+      if (err.code == 'failed-precondition') {
+        // Multiple tabs open, persistence can only be enabled
+        // in one tab at a time.
+        console.warn('Firestore persistence failed: failed-precondition. Multiple tabs open?');
+      } else if (err.code == 'unimplemented') {
+        // The current browser does not support all of the
+        // features required to enable persistence
+        console.warn('Firestore persistence failed: unimplemented. Browser not supported?');
+      }
+    });
+}
+
 
 export { app, db };
