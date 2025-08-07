6f29ad8ba3d8724690c4924691998458f9f67e22
Fernando Pbt <fernandopbt450@gmail.com>
2025-08-07 20:05:46 +0000

    Ready condition status changed to True for Service studio. Propiedades {

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -15,11 +15,6 @@ const firebaseConfig = {
   "messagingSenderId": "220291714642"
 };
 
-// Conditional configuration for the development environment
-if (typeof window !== 'undefined' && window.location.hostname.includes('cloudworkstations.dev')) {
-    firebaseConfig.authDomain = window.location.hostname;
-}
-
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
