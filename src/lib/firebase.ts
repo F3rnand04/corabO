163551628a9641c66510bec4bb6a2c35ad3cbc2a
Fernando Pbt <fernandopbt450@gmail.com>
2025-08-07 19:15:27 +0000

    puedes validar las rutas en una prueba aislada desde aqui

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,5 @@
 
-
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
@@ -10,14 +9,15 @@ const firebaseConfig = {
   "appId": "1:220291714642:web:3aca123e39a92f16c0998b",
   "storageBucket": "corabo-demo.firebasestorage.app",
   "apiKey": "AIzaSyAOZ9eRQz1Sry6pdLNwCVZ3QNsr1pZgHnQ",
-  "authDomain": "corabo-demo.us-central1.hosted.app",
+  // Forzamos el dominio de autenticación para que coincida con el dominio de despliegue
+  "authDomain": "studio--corabo-demo.us-central1.hosted.app", 
   "measurementId": "",
   "messagingSenderId": "220291714642"
 };
 
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const provider = new GoogleAuthProvider();
