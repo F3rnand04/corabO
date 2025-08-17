91d5925633a4ef1047669958a038854accae1bcb
Fernando Pbt <fernandopbt450@gmail.com>
2025-08-17 00:10:50 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      }
+    ],
+  },
   webpack: (config) => {
     // Tell webpack to ignore watching the files that Genkit generates.
     // This prevents an infinite hot-reload loop.
