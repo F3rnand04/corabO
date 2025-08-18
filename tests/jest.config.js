b3849976108834d15523a294f4ac6686833bb294
Fernando Pbt <fernandopbt450@gmail.com>
2025-08-18 14:33:37 +0000

    PERFECTO, VERIFICA QUE TODO SEA FUNCIONAL PARA EL DESPLIEGUE DAME ESTADO

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -6,8 +6,20 @@ module.exports = {
   // Se especifica explícitamente dónde buscar los archivos de prueba.
   roots: ['<rootDir>'], 
   moduleNameMapper: {
+    // Apunta a la carpeta 'src' del proyecto, no a un nivel superior.
     '^@/(.*)$': '<rootDir>/../src/$1',
+    // Mock para archivos CSS, específico para el entorno de pruebas.
     '\\.css$': '<rootDir>/styleMock.js',
+  },
+  // Ignora la transformación de todos los módulos de node_modules excepto 'yaml'
+  transformIgnorePatterns: [
+    '/node_modules/(?!yaml)/'
+  ],
+  // Define cómo transformar diferentes tipos de archivos para Jest.
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest', // Usa babel-jest para archivos JS/JSX si es necesario
+    '^.+\\.mjs$': 'ts-jest',
   },
   testPathIgnorePatterns: [
     '<rootDir>/../.next/',
