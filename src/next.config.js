e6700dc7303eec26dadc9cfdc908f3a5f2a5c2b4
Fernando Pbt <fernandopbt450@gmail.com>
2025-08-20 17:26:20 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/next.config.js
+++ src/next.config.js
@@ -17,7 +17,7 @@ const nextConfig = {
       }
     ],
   },
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     // Tell webpack to ignore watching the files that Genkit generates.
     // This prevents an infinite hot-reload loop.
     config.watchOptions = {
@@ -28,6 +28,12 @@ const nextConfig = {
         "**/firebase-debug.log",
       ],
     };
+    
+    // FIX: Add problematic dependencies to externals to prevent webpack bundling errors.
+    if (isServer) {
+        config.externals = [...config.externals, "handlebars", "require-in-the-middle"];
+    }
+    
     return config;
   },
 };
