551434a0aa78fbf7d279de2b9870761e1f0e8da5
Fernando Pbt <fernandopbt450@gmail.com>
2025-08-27 19:41:34 +0000

    Prompt para Diagnóstico de Fallos de Red y Estabilización Final Instrucc

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -5,7 +5,7 @@
 export const firebaseConfig = {
   "projectId": "corabo-demo",
   "appId": "1:220291714642:web:3aca123e39a92f16c0998b",
-  "storageBucket": "corabo-demo.firebasestorage.app",
+  "storageBucket": "corabo-demo.appspot.com",
   "apiKey": "AIzaSyAOZ9eRQz1Sry6pdLNwCVZ3QNsr1pZgHnQ",
   "authDomain": "corabo-demo.firebaseapp.com",
   "measurementId": "G-CYN8E0S2VZ",
