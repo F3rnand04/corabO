727c591bcc8c8bdea63f7758d4f7a31fbe48967a
Fernando Pbt <fernandopbt450@gmail.com>
2025-09-18 21:44:45 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/app/cashier-login/page.tsx
+++ src/app/cashier-login/page.tsx
@@ -1,10 +1,8 @@
 
 'use client';
 
-import { CashierLogin } from "@/components/auth/CashierLogin";
-
 // This page is currently not in use and has been simplified.
 // The cashier login flow has been removed to reduce complexity.
 export default function CashierLoginPage() {
-    return <CashierLogin />;
+    return null;
 }
