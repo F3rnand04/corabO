36e29f3cf124f3e765c44656f9039b4193c90386
Fernando Pbt <fernandopbt450@gmail.com>
2025-09-22 17:14:38 +0000

    si esta en la lista autorizada y aqui te dejo lo demas para que comoprar

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -6,7 +6,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyAOZ9eRQz1Sry6pdLNwCVZ3QNsr1pZgHnQ",
   authDomain: "corabo-demo.firebaseapp.com",
   projectId: "corabo-demo",
-  storageBucket: "corabo-demo.appspot.com",
+  storageBucket: "corabo-demo.firebasestorage.app",
   messagingSenderId: "220291714642",
   appId: "1:220291714642:web:3aca123e39a92f16c0998b",
   measurementId: "G-CYN8E0S2VZ"
